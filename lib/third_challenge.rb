9917d8b01225b3374d107df845b7ba84f150a40e
Stephen Olson <stephenolson@gmail.com>
2018-10-04 21:12:36 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,20 +17,20 @@ def third_challenge
       hero_friends: []
    }, 
    capulet: {
-      :patriarch => {
+      patriarch: {
         name: "Lord Capulet",
         age: "50"
         },
-      :matriarch => {
+      matriarch: {
         name: "Lady Capulet",
         age: "51"
         },
-      :heroine => {
+      heroine: {
         name: "Juliet",
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      heroine_friends: []
    }
   }
 
