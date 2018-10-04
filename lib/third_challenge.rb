2628804c37d8ab2cc69c3330e80b393798f2f052
Stephen Olson <stephenolson@gmail.com>
2018-10-04 21:12:03 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,22 +1,22 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
+   montague: {
+      patriarch: {
         name: "Lord Montague",
         age: "53"
         },
-      :matriarch => {
+      matriarch: {
         name: "Lady Montague",
         age: "54"
         },
-      :hero => {
+      hero: {
         name: "Romeo",
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
+      hero_friends: []
    }, 
-   :capulet => {
+   capulet: {
       :patriarch => {
         name: "Lord Capulet",
         age: "50"
